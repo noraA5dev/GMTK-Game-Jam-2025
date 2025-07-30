03990ac52b075154990b877809c3b15351476bf9
noraA5dev <aaron.gami.art@gmail.com>
2025-07-30 18:44:41 -0500

    fixed last commit

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
 
 const MAX_SPEED = 300.0
 const JUMP_VELOCITY = -300.0
@@ -13,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		death()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,3 +28,5 @@ func _physics_process(delta: float) -> void:
 		SPEED = 0
 
 	move_and_slide()
+func death():
+	print(r"¯\_(ツ)_/¯")
