c00a05e13ac17ae57f5fdd7b40f4f500315f6291
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-08-01 16:05:02 -0500

    fixed the spawning script

--- Scenes/tile_generator_area.gd
+++ Scenes/tile_generator_area.gd
@@ -26,6 +26,7 @@ func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
 	var test = get_tree().root.get_node("World/5 Wide")
 	print(test)
 	Tile_Spacing_X = test.tile_spacing_x * 8
-	Tile_Spacing_Y = test.tile_spacing_y * 8
+	#Tile_Spacing_Y = test.tile_spacing_y * 8
 	current_instanced_tile.global_position = Vector2(self.global_position.x + Tile_Spacing_X, self.global_position.y+ Tile_Spacing_Y)
-	self.global_position = Vector2(current_instanced_tile.global_position.x, current_instanced_tile.global_position.y)
+	#self.global_position = Vector2(current_instanced_tile.global_position.x, current_instanced_tile.global_position.y)
+	self.global_position.x = current_instanced_tile.global_position.x
