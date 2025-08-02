b02f663fd366d021daa3b590117fde183f1efede
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-08-02 09:14:54 -0500

    some generation things

--- Scenes/tile_generator_area.gd
+++ Scenes/tile_generator_area.gd
@@ -1,6 +1,6 @@
 extends Marker2D
 
-const _5_WIDE = preload("res://Maps/5_wide.tscn")
+const _5_WIDE = preload("res://Maps/map_2.tscn")
 @onready var world_node: Node2D = $".."
 
 var current_instanced_tile: Marker2D = null
@@ -18,14 +18,13 @@ func _process(_delta: float) -> void:
 
 func get_next_tile():
 	return _5_WIDE
-func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
-	print("Test")
+func _on_area_2d_body_entered(_body: CharacterBody2D) -> void:
 	current_tile = get_next_tile().instantiate()
 	world_node.add_child(current_tile)
 	current_instanced_tile = world_node.get_child(-1)
-	var test = get_tree().root.get_node("World/5 Wide")
+	var test = get_tree().root.get_node("res://Maps/map_2.gd")
 	print(test)
-	Tile_Spacing_X = test.tile_spacing_x * 8
+	#Tile_Spacing_X = test.tile_spacing_x * 8
 	#Tile_Spacing_Y = test.tile_spacing_y * 8
 	current_instanced_tile.global_position = Vector2(self.global_position.x + Tile_Spacing_X, self.global_position.y+ Tile_Spacing_Y)
 	#self.global_position = Vector2(current_instanced_tile.global_position.x, current_instanced_tile.global_position.y)
