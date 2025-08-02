68e6761c450433d1e6ea688f4e1ea93a2fcaa9d5
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-08-02 13:57:20 -0500

    ¯\_(ツ)_/¯

--- Scenes/tile_generator_area.gd
+++ Scenes/tile_generator_area.gd
@@ -18,13 +18,12 @@ func _process(_delta: float) -> void:
 
 func get_next_tile():
 	return _5_WIDE
-func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
-	print("Test")
+func _on_area_2d_body_entered(_body: CharacterBody2D) -> void:
 	current_tile = get_next_tile().instantiate()
 	world_node.add_child(current_tile)
 	current_instanced_tile = world_node.get_child(-1)
 	var test = get_tree().root.get_node("World/5 Wide")
-	print(test)
+	print(test.tile_spacing_x, ", ", test.tile_spacing_y)
 	Tile_Spacing_X = test.tile_spacing_x * 8
 	current_instanced_tile.global_position = Vector2(self.global_position.x + Tile_Spacing_X, self.global_position.y)
 	self.global_position = Vector2(current_instanced_tile.global_position.x, current_instanced_tile.global_position.y)
