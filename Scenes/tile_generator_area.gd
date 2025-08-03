0cf08e389266663bde71cbe2451d712174a67a34
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-08-03 11:39:54 -0500

    getting the fufing generation to work

--- Scenes/tile_generator_area.gd
+++ Scenes/tile_generator_area.gd
@@ -2,8 +2,8 @@ extends Marker2D
 
 @onready var world_node: Node2D = $".."
 @onready var map_num: int = $".".map_number
-var maps = {1: {"pre": preload("res://Maps/5_wide.tscn"), "node": Object, "node_tree": "/World/5 wide"},
-			2: {"pre": preload("res://Maps/map_2.tscn"), "node": Object, "node_tree": "/World/map_2"}}
+var maps = {1: preload("res://Maps/5_wide.tscn"),
+			2: preload("res://Maps/map_2.tscn")}
 var current_instanced_tile: Marker2D = null
 var current_tile: Marker2D = null
 var Tile_Spacing_X: int = 0
@@ -19,23 +19,31 @@ func _process(_delta: float) -> void:
 	pass
 
 func get_next_tile():
-	return maps[map_num]["pre"]
+	#return maps[map_num]["pre"]
+	var keys = maps.keys()
+	var random_index = randi() % keys.size()
+	var random_key = keys[random_index]
+	var random_value = maps[random_key]
+	return random_value
 
 func _on_area_2d_body_entered(_body: CharacterBody2D) -> void:
-	print(maps)
-	print(map_num)
+	print(get_next_tile())
+	#print(maps)
+	#print(map_num)
 	current_tile = get_next_tile().instantiate()
 	world_node.add_child(current_tile)
 	current_instanced_tile = world_node.get_child(-1)
-	maps[map_num]["node"] = get_tree().root.get_node(maps[map_num]["node_tree"])
-	var test = maps[map_num]["node"]
-	print(test)
+	
+	
+	#maps[map_num]["node"] = get_tree().root.get_node(maps[map_num]["node_tree"])
+	#var test = maps[map_num]["node"]
+	#print(test)
 	
 	# debug
 	print(current_tile)
 	print(current_instanced_tile)
+	Tile_Spacing_X = current_instanced_tile.tile_spacing_x * 8 
+	print(Tile_Spacing_X, ", ", Tile_Spacing_Y)
 	
-	#print(test.tile_spacing_x, ", ", test.tile_spacing_y)
-	Tile_Spacing_X = 50 * 8
 	current_instanced_tile.global_position = Vector2(self.global_position.x + Tile_Spacing_X, self.global_position.y)
 	self.global_position = Vector2(current_instanced_tile.global_position.x, current_instanced_tile.global_position.y)
